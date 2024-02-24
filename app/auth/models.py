fe86d4aa1b04b5e312409be52dac8bd0adbc430e
Elijah Mylcev <elijahmylcev@gmail.com>
2024-02-24 19:46:28 +0100

    Added health check

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,27 +1,13 @@
-from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy.orm import DeclarativeBase
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID
 
-from sqlalchemy import JSON, Column, ForeignKey, Integer, String, TIMESTAMP, Enum
-from enum import Enum as PyEnum
+from sqlalchemy import Column, ForeignKey, Integer, String, TIMESTAMP
 from datetime import datetime
 from app.models.models import role
 
 
 class Base(DeclarativeBase):
     pass
-
-#
-# class RoleEnum(PyEnum):
-#     PRODUCER = "producer"
-#     CONSUMER = "consumer"
-#
-#
-# class Role(Base):
-#     __tablename__ = "role"
-#
-#     id = Column(Integer, primary_key=True, index=True)
-#     name = Column(Enum(RoleEnum), unique=True)
-#     permissions = Column(JSON)
 
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
