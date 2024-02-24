4fad372de15e8f37bb6252b9943b522e78a026ed
Elijah Mylcev <elijahmylcev@gmail.com>
2024-02-24 22:53:29 +0100

    origins

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,19 @@ from fastapi.middleware.cors import CORSMiddleware
 
 def create_app(_=None) -> FastAPI:
     app = FastAPI()
+
+    origins = [
+        "http://localhost.tiangolo.com",
+        "https://localhost.tiangolo.com",
+        "http://localhost",
+        "http://localhost:8080",
+        "https://localhost:5173",
+        "http://localhost:5173"
+    ]
+
     app.add_middleware(
         CORSMiddleware,
-        allow_origins='*',
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=['*'],
         allow_headers=['*']
